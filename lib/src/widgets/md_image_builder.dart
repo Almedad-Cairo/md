25fd028ccb7a64cf20a7359bed7c9ecfcc5bf65e
Ahmed Habeeb <ahmedhabeeb1117@gmail.com>
2024-06-29 10:42:16 +0300

    mdImageBuilder

--- lib/src/widgets/md_image_builder.dart
+++ lib/src/widgets/md_image_builder.dart
@@ -5,38 +5,31 @@ import 'package:path_provider/path_provider.dart';
 
 import '../../md_framework.dart';
 
-@Deprecated('Use MDImageBuilder instead')
-class MDImage extends StatefulWidget {
+class MDImageBuilder extends StatefulWidget {
   final String fileId;
 
-  final BoxFit fit;
-  final double? width, height;
-  final Widget? loadingWidget;
-  final Widget? errorWidget;
-  final Alignment alignment;
-  final ImageFrameBuilder? frameBuilder;
-  final ImageErrorWidgetBuilder? errorBuilder;
-  final BoxDecoration? decoration;
+  // builder
+  final Widget Function(BuildContext context, File file) builder;
 
-  const MDImage({
+  // loadingWidget
+  final Widget? loadingWidget;
+
+  // errorWidget
+  final Widget? Function(Object error)? errorWidget;
+
+  const MDImageBuilder({
     Key? key,
     required this.fileId,
-    this.width = 50,
-    this.height = 50,
-    this.errorBuilder,
-    this.frameBuilder,
+    required this.builder,
     this.loadingWidget,
     this.errorWidget,
-    this.decoration,
-    this.alignment = Alignment.center,
-    this.fit = BoxFit.contain,
   }) : super(key: key);
 
   @override
-  State<MDImage> createState() => _MDImageState();
+  State<MDImageBuilder> createState() => _MDImageBuilderState();
 }
 
-class _MDImageState extends State<MDImage> {
+class _MDImageBuilderState extends State<MDImageBuilder> {
   late Future<File> fileFuture;
 
   @override
@@ -106,31 +99,20 @@ class _MDImageState extends State<MDImage> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return widget.loadingWidget ??
               const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          // throw Exception('Error downloading file: ${snapshot.error}');
-          return widget.errorWidget ??
-              Center(child: Text("Error: ${snapshot.error}"));
-        } else {
-          return Container(
-            width: widget.width,
-            height: widget.height,
-            decoration: widget.decoration == null
-                ? BoxDecoration(
-                    image: DecorationImage(
-                      image: FileImage(snapshot.data!),
-                      fit: widget.fit,
-                      alignment: widget.alignment,
-                    ),
-                  )
-                : widget.decoration!.copyWith(
-                    image: DecorationImage(
-                      image: FileImage(snapshot.data!),
-                      fit: widget.fit,
-                      alignment: widget.alignment,
-                    ),
-                  ),
-          );
         }
+        if (snapshot.hasError) {
+          return widget.errorWidget != null
+              ? widget.errorWidget!(snapshot.error!)!
+              : const Center(
+                  child: Text('Error loading image'),
+                );
+        }
+        if (snapshot.hasData) {
+          return widget.builder(context, snapshot.data!);
+        }
+        return const Center(
+          child: Text('Error loading image'),
+        );
       },
     );
   }
